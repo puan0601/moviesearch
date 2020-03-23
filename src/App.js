eec37be62a64075611e31caf871b4c09b2fcbea3
puan0601 <anton.pugach@gmail.com>
2020-03-23 15:35:32 -0700

    gets search api working with input field and renders results with css grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+const URL = 'https://api.themoviedb.org/3/';
+const KEY = '72488761d7135320d1cbbec467fac5a8';
 
 class App extends Component {
   state = {
-    popularList: []
+    popularList: [],
+    input: '',
+    selectedItemID: null,
+    data: []
   };
 
-  componentDidMount() {
-    const POPULAR = 'https://api.themoviedb.org/3/movie/popular';
-    const KEY = '72488761d7135320d1cbbec467fac5a8';
+  handleChange = (event) => {
+    const { value } = event.target;
 
-    fetch(`${POPULAR}?api_key=${KEY}`)
+    this.setState({ input: value });
+
+    fetch(`${URL}search/movie?api_key=${KEY}&query=${value}`)
       .then(response => response.json())
+      .then(data => this.setState({ data: data.results }))
+  }
+
+  // grabs top 10 popular movies list from themoviedb.org 
+  componentDidMount() {
+    fetch(`${URL}movie/popular?api_key=${KEY}`)
+      .then(response => response.json()) 
       .then(data => this.setState({ popularList: data.results.splice(0,10) })) 
   }
   
   render() {
-    const { popularList } = this.state;
-    
+    const { popularList, data, input } = this.state;
+
     return (
       <div className="App">
         <h1>Search any movie!</h1>
@@ -27,6 +40,16 @@ class App extends Component {
             return <div className="item" key={movie.id}>{movie.title}</div>
           })}
         </div>
+
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input autoFocus value={this.state.input} placeholder="Enter Movie Title" onChange={this.handleChange} />
+        </form> 
+        {input && <h3>Results:</h3>}
+        <div className="container">
+          {data && data.map(movie => {
+            return <div className="item" key={movie.id}>{movie.title}</div>
+          })}
+        </div>     
       </div>
     )  
   }
