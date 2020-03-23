effad6dbcc0ce5fe1a49d2f53a81262a7f1b0658
puan0601 <anton.pugach@gmail.com>
2020-03-22 19:52:28 -0700

    renders top 20 popular movies list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    popularList: []
+  };
 
+  componentDidMount() {
+    const POPULAR = 'https://api.themoviedb.org/3/movie/popular';
+    const KEY = '72488761d7135320d1cbbec467fac5a8';
+
+    fetch(`${POPULAR}?api_key=${KEY}`)
+      .then(response => response.json())
+      .then(data => this.setState({ popularList: data.results })) 
+  }
+  
+  render() {
+    const { popularList } = this.state;
+    
+    return (
+      <div className="App">
+        <ol>
+          {popularList.map(movie => {
+            return <li key={movie.id}>{movie.title}</li>
+          })}
+        </ol>
+      </div>
+    )  
+  }
+};
+  
 export default App;
