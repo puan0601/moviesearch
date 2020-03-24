33c33223ddeb0d8b488d5ad4ec1f098616402236
puan0601 <anton.pugach@gmail.com>
2020-03-24 16:05:21 -0700

    debounces getData API call 500ms

--- src/App.js
+++ src/App.js
@@ -1,53 +1,110 @@
 import React, { Component } from 'react';
 import './App.css';
-const URL = 'https://api.themoviedb.org/3/';
+import debounce from 'lodash/debounce';
+
+const API_URL = 'https://api.themoviedb.org/3/';
+const IMG_URL = 'https://image.tmdb.org/t/p/w200/';
 const KEY = '72488761d7135320d1cbbec467fac5a8';
 
 class App extends Component {
-  state = {
-    popularList: [],
-    input: '',
-    selectedItemID: null,
-    data: []
-  };
+  constructor(props) {
+    super(props);
 
-  handleChange = (event) => {
+    this.state = {
+      popularList: [],
+      popularListRender: false,
+      input: '',
+      selectedItemID: null,
+      data: [],
+    };
+
+    this.debounced = debounce(this.getData, 1000);
+  }
+
+  handleInputChange = (event) => {
     const { value } = event.target;
 
-    this.setState({ input: value });
+    this.setState({ 
+      input: value,
+    });
 
-    fetch(`${URL}search/movie?api_key=${KEY}&query=${value}`)
+    this.debounced(value);    
+  }
+
+  handleItemClick = (event) => {
+    this.setState({ selectedItemID: parseInt(event.target.getAttribute('itemID')) });
+  }
+
+  getData = (value) => {
+    fetch(`${API_URL}search/movie?api_key=${KEY}&query=${value}`)
       .then(response => response.json())
-      .then(data => this.setState({ data: data.results }))
+      .then(data => this.setState({
+        data: data.results
+      }))
   }
 
   // grabs top 10 popular movies list from themoviedb.org 
   componentDidMount() {
-    fetch(`${URL}movie/popular?api_key=${KEY}`)
+    fetch(`${API_URL}movie/popular?api_key=${KEY}`)
       .then(response => response.json()) 
-      .then(data => this.setState({ popularList: data.results.splice(0,10) })) 
+      .then(data => this.setState({ 
+        popularList: data.results.splice(0,10),
+        popularListRender: true  
+      })) 
   }
   
   render() {
-    const { popularList, data, input } = this.state;
+    const { popularList, popularListRender, data, selectedItemID, input } = this.state;
 
     return (
       <div className="App">
-        <h1>Search any movie!</h1>
-        <h2>Most Popular Movies of Today</h2>
-        <div className="container">
+        <h1>Search any movie below</h1>
+        
+        {popularListRender && <h2>Most Popular Movies of Today</h2>}
+        <div className="popularContainer">
           {popularList.map(movie => {
-            return <div className="item" key={movie.id}>{movie.title}</div>
+            return <div className="popularItem" 
+                        key={movie.id}
+                    >
+                        {movie.title}
+                    </div>
           })}
         </div>
 
         <form onSubmit={(e) => e.preventDefault()}>
-          <input autoFocus value={this.state.input} placeholder="Enter Movie Title" onChange={this.handleChange} />
+          <input autoFocus 
+                value={this.state.input} 
+                placeholder="Enter Movie Title" 
+                onChange={this.handleInputChange} />
         </form> 
-        {input && <h3>Results:</h3>}
-        <div className="container">
+        
+        {input && data && <h3>Results ({data.length})</h3>}
+        <div className="dataContainer">
           {data && data.map(movie => {
-            return <div className="item" key={movie.id}>{movie.title}</div>
+            if (selectedItemID === movie.id) {
+              return (
+                <div className="item"
+                  id="selected"
+                  key={movie.id}
+                  itemID={movie.id}
+                  onClick={this.handleItemClick}
+                >
+                  <h1>{movie.title}</h1>
+                  <p><b>Release Date:</b> {movie.release_date}</p>
+                  <p><b>Overview:</b> {movie.overview}</p>
+                  <img alt={movie.title} src={`${IMG_URL}${movie.poster_path}`} />
+                </div> 
+              )
+            }
+            return (
+              <div className="item" 
+                        key={movie.id}
+                        itemID={movie.id}
+                        onClick={this.handleItemClick}
+                    >
+                        {movie.title}
+              </div>
+            );
           })}
         </div>     
       </div>
